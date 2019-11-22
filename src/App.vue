ecd2fe87dbc50d8dad5278cbe56dbac4454ae951
berryllium <gorkundp@yandex.ru>
2019-11-22 21:34:58 +0300

    стили

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,28 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <Header />
+    <Catalog />
+    <router-view />
+    <Footer />
   </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+import Header from "@/components/Header";
+import Catalog from "@/components/Catalog";
+import Footer from "@/components/Footer";
+export default {
+  components: {
+    Header,
+    Footer
   }
-}
+};
+</script>
+
+<style lang="less">
+@import url("style/font-awesome.min.css");
+@import url("style/jquery.fancybox.css");
+@import url("style/normalize.css");
+@import url("style/variables.less");
+@import url("style/main.less");
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,43 @@
+<template>
+  <header>
+    <div class="container">
+      <nav id="menu">
+        <router-link class="menu__item" to="/">Главная</router-link>
+        <router-link class="menu__item" to="/">О нас</router-link>
+        <router-link class="menu__item" to="/">Каталог</router-link>
+        <router-link class="menu__item" to="/">Доставка и оплата</router-link>
+        <router-link class="menu__item" to="/">Контакты</router-link>
+        <router-link class="menu__item" to="/">Гаратния</router-link>
+        <router-link class="menu__item" to="/">Отзывы</router-link>
+      </nav>
+      <div class="header-info">
+        <div class="header-info__item"><img src="http://placehold.it/100x100" /></div>
+        <div class="header-info__item"><i class="fa fa-map-marker" aria-hidden="true"></i></div>
+        <div class="header-info__item"></div>
+        <div class="header-info__item"></div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style lang="less" scoped>
+@import url("../style/variables.less");
+#menu {
+  display: flex;
+  height: 65px;
+  line-height: 65px;
+  border-bottom: 2px solid #e1e1e1;
+  justify-content: space-between;
+  .menu__item {
+    text-transform: uppercase;
+    text-decoration: none;
+    font-size: 0.9em;
+    color: #666;
+    padding: 0;
+    &:hover {
+      transform: scale(1.05);
+      color: @blue;
+    }
+  }
+}
+</style>
