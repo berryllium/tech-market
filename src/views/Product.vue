98a0bcbf32bee11f8ea7365f6daaae12378b6b7b
berryllium <gorkundp@yandex.ru>
2019-12-02 23:36:25 +0300

    верстка страницы товара

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -42,8 +42,113 @@
         </div>
       </div>
     </section>
+    <section class="section-tabs">
+      <div class="tabs">
+        <ul class="tabs__header">
+          <li
+            class="tabs__link"
+            :class="this.tab == 'desc'?'active':''"
+            @click="changeTab('desc')"
+          >Описание</li>
+          <li
+            class="tabs__link"
+            :class="this.tab == 'prop'?'active':''"
+            @click="changeTab('prop')"
+          >Характеристики</li>
+          <li
+            class="tabs__link"
+            :class="this.tab == 'guar'?'active':''"
+            @click="changeTab('guar')"
+          >Гарантия</li>
+          <li
+            class="tabs__link"
+            :class="this.tab == 'deliv'?'active':''"
+            @click="changeTab('deliv')"
+          >Доставка и оплата</li>
+          <li
+            class="tabs__link"
+            :class="this.tab == 'feed'?'active':''"
+            @click="changeTab('feed')"
+          >Отзывы</li>
+        </ul>
+        <div class="tabs__content">
+          <div
+            class="tabs__item"
+            v-if="this.tab=='desc'"
+          >Описания Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut hic, dicta molestias asperiores in ea sequi? Natus laboriosam voluptatem explicabo.</div>
+          <div class="tabs__item" v-if="this.tab=='prop'">
+            <table class="tabs__table">
+              <tr>
+                <td>Поле</td>
+                <td>Значение</td>
+              </tr>
+              <tr>
+                <td>Поле</td>
+                <td>Значение</td>
+              </tr>
+              <tr>
+                <td>Поле</td>
+                <td>Значение</td>
+              </tr>
+              <tr>
+                <td>Поле</td>
+                <td>Значение</td>
+              </tr>
+              <tr>
+                <td>Поле</td>
+                <td>Значение</td>
+              </tr>
+            </table>
+          </div>
+          <div class="tabs__item" v-if="this.tab=='guar'">
+            <h4>Гарантия от производителя</h4>
+            <li>На данную модель гироскутера распространяется официальная гарантия от производителя на бесплатный ремонт и обслуживание в течении 12 месяцев.</li>
+            <li>Гироскутер не подлежит гарантии если было самовольное вскрытие устройства.</li>
+          </div>
+          <div class="tabs__item" v-if="this.tab=='deliv'">
+            <h4>Доставка по Москве</h4>
+            <p>Для клиентов наш интернет-салон открыт круглые сутки. Если Вы делаете заказ в Москве, наш курьер доставит Ваше приобретение всего в течение часа. Вы можете позвонить нам по телефону, а также сделать заказ через онлайн-форму на сайте. Все это бесплатно, удобно и экономит Ваше время.</p>
+          </div>
+          <div class="tabs__item" v-if="this.tab=='feed'">
+            <div class="feedback-wrap">
+              <div class="feedback">
+                <header>
+                  <div class="name">Андрей</div>
+                  <div class="date">29.12.2019</div>
+                  <div class="stars stars_four">
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star-o" aria-hidden="true"></i>
+                  </div>
+                  <div
+                    class="text"
+                  >Долго держит заряд акб, если еще комплект ручки с сиденьем купить, получается практически картинг)</div>
+                </header>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      tab: "desc"
+    };
+  },
+  methods: {
+    changeTab(tab) {
+      this.tab = tab;
+    }
+  }
+};
+</script>
 
 <style lang="less" scoped>
 @import url("../style/variables.less");
@@ -140,6 +245,50 @@ h1 {
   &__content {
     padding: 10px;
     text-align: justify;
+  }
+}
+.tabs {
+  border: 1px solid @blue;
+
+  .tabs__header {
+    display: flex;
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+    border-bottom: 1px solid @blue;
+  }
+  .tabs__link {
+    padding: 10px 0;
+    text-align: center;
+    flex: 1;
+    transition-duration: 0.3s;
+    &:hover,
+    &.active {
+      background-color: @blue;
+      color: white;
+      cursor: pointer;
+    }
+  }
+  .tabs__content {
+    padding: 10px;
+  }
+  .tabs__table {
+    border-collapse: collapse;
+    width: 100%;
+    th,
+    td {
+      padding: 5px;
+    }
+    td {
+      width: 50%;
+    }
+    th {
+      background-color: lighten(@grey, 40%);
+    }
+  }
+  .fa-star,
+  .fa-star-o {
+    color: gold;
   }
 }
 </style>
