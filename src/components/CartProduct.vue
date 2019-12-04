85cd86ef812584f6c5f049d60af0fe793c69fdec
berryllium <gorkundp@yandex.ru>
2019-12-04 19:39:38 +0300

    сверстал корзину

---
+++ src/components/CartProduct.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="cart-product">
+    <img src="images/image-phone.jpg" alt="photo" class="cart-product__img">
+    <a href="/product" class="cart-product__name">Название товара</a>
+    <div class="cart-product__count">
+      <input type="number" min="1" name="count" class="cart-product__input" id="count" value="1">
+      <label for="count"> шт.</label>
+    </div>
+    <div class="cart-product__price">5800 руб.</div>
+    <button class="cart-product__remove">&times;</button>
+  </div>
+</template>
+<style lang="less" scoped>
+@import url('../style/variables.less');
+  .cart-product {
+    display: grid;
+    grid-template-columns: 30% 35% 15% 15% 5%;
+    align-items: center;
+    margin-bottom: 20px;
+    &__img {
+      width: 150px;
+      height: 150px;
+      object-fit: contain;
+    }
+    &__input {
+      width: 50px;
+    }
+    &__remove {
+      margin-left: auto;
+      border: none;
+      outline: none;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 30px;
+      height: 30px;
+      font-size: 1.5em;
+      background: @grey;
+      border-radius: 50%;
+      color: white;
+      &:hover {
+        cursor: pointer;
+        background-color: red;
+      }
+      &:active {
+        transform: scale(.95)
+      }
+    }
+  }
+</style>
--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,8 +1,83 @@
 <template>
   <div class="content">
-    <h1>Корзина</h1>
-        <div class="page-text">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti sapiente dignissimos quam, expedita facere odit totam sunt recusandae. Dolorum dignissimos ipsum quam neque, libero facilis, ea ad minima suscipit aspernatur, ab porro? Nesciunt ut possimus assumenda totam ipsum, error fugiat deleniti placeat cumque quo sapiente numquam earum aperiam architecto adipisci! Hic sit qui facere officiis possimus veniam laborum recusandae iste voluptas ratione eius, molestias dignissimos? Dignissimos, aspernatur aliquam! Accusamus accusantium in iste esse, harum aut praesentium alias obcaecati fuga eveniet eum ipsam explicabo tenetur placeat inventore! Cumque, inventore. Sunt dicta deserunt minus non quae veniam ad sequi architecto eos laudantium?
+    <h1>Корзина товаров</h1>
+    <div class="cart-products">
+      <cart-product />
+      <cart-product />
+    </div>
+    <div class="amount">
+      Итого:
+      <span class="sum">11600</span> руб.
+    </div>
+    <div class="form">
+      <h3 class="form__header">Оформление заказа</h3>
+      <div class="form__content">
+        <div class="form__user">
+          <label for="name">Ваше имя</label>
+          <input type="name" class="form__name" name="name" placeholder="Имя" />
+          <label for="email">Ваш email</label>
+          <input
+            type="email"
+            class="form__email"
+            name="email"
+            placeholder="Адрес электронной почты"
+          />
+          <label for="phone">Ваш телефон</label>
+          <input type="phone" class="form__phone" name="phone" placeholder="Номер телефона" />
+          <label for="address">Ваш адрес</label>
+          <textarea rows="4" class="form__address" name="address" placeholder="Адрес доставки"></textarea>
+        </div>
+      </div>
+        <submit class="form__button">Заказать</submit>
     </div>
   </div>
 </template>
+<script>
+import CartProduct from "@/components/CartProduct";
+export default {
+  components: {
+    "cart-product": CartProduct
+  }
+};
+</script>
+<style lang="less" scoped>
+@import url('../style/variables.less');
+.amount {
+  text-align: right;
+  font-size: 1.2em;
+  span {
+    font-weight: bold;
+  }
+}
+.form {
+  margin: 0 auto;
+  width: 50%;
+  &__header {
+    margin: 20px 0;
+  }
+  input,
+  textarea {
+    box-sizing: border-box;
+    width: 100%;
+    padding: 5px;
+    margin: 5px 0 10px 0;
+  }
+  &__button {
+    display: block;
+    margin-top: 20px;
+    text-align: center;
+    border: none;
+    outline: none;
+    padding: 10px;
+    background-color: @blue;
+    color: white;
+    &:hover {
+      cursor: pointer;
+      background-color: darken(@blue, 10%);
+    }
+    &:active {
+      transform: scale(0.95);
+    }
+  }
+}
+</style>
