d736fa2b5e934f3bbe7fb3dfe2b3569a1d618b25
berryllium <gorkundp@yandex.ru>
2019-12-05 11:27:28 +0300

    контакты

--- src/views/Contacts.vue
+++ src/views/Contacts.vue
@@ -1,8 +1,93 @@
 <template>
   <div class="content">
     <h1>Контакты</h1>
-        <div class="page-text">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti sapiente dignissimos quam, expedita facere odit totam sunt recusandae. Dolorum dignissimos ipsum quam neque, libero facilis, ea ad minima suscipit aspernatur, ab porro? Nesciunt ut possimus assumenda totam ipsum, error fugiat deleniti placeat cumque quo sapiente numquam earum aperiam architecto adipisci! Hic sit qui facere officiis possimus veniam laborum recusandae iste voluptas ratione eius, molestias dignissimos? Dignissimos, aspernatur aliquam! Accusamus accusantium in iste esse, harum aut praesentium alias obcaecati fuga eveniet eum ipsam explicabo tenetur placeat inventore! Cumque, inventore. Sunt dicta deserunt minus non quae veniam ad sequi architecto eos laudantium?
+    <div class="contacts">
+      <div class="address">
+        <div class="info__item">
+          <i class="fa fa-map-marker left-icon" aria-hidden="true"></i>
+          <p>Москва, ул. Лермонтова</p>
+        </div>
+        <div class="info__item">
+          <i class="fa fa-phone left-icon" aria-hidden="true"></i>
+          <p>
+            <a href="tel: 89999999999">8(999)999-99-99</a>
+          </p>
+          <p>
+            <a href="tel: 89999999999">8(999)999-99-99</a>
+          </p>
+          <div class="soc-icons">
+            <a href="#">
+              <i class="fa fa-whatsapp" aria-hidden="true"></i>
+            </a>
+            <a href="#">
+              <i class="fa fa-telegram" aria-hidden="true"></i>
+            </a>
+          </div>
+        </div>
+        <div class="info__item">
+          <i class="fa fa-envelope-o left-icon" aria-hidden="true"></i>
+          <p><a href="mailto: email@yandex.ru">email@yandex.ru</a></p>
+        </div>
+        <div class="info__item">
+          <i class="fa fa-clock-o left-icon" aria-hidden="true"></i>
+          <p>
+            Прием заказов и доставка
+            <b>8.00-24.00</b>
+          </p>
+          <p>
+            Самовывоз
+            <b>10.00-19.00</b>
+          </p>
+          <p>Работаем без выходных</p>
+        </div>
+      </div>
+      <div class="map">
+        <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Af1ee48f526c937a08a9f4716c0d01103b6a37a027f1f39c5f92f73c0b9a84252&amp;source=constructor" width="550" height="350" frameborder="0"></iframe>
+      </div>
     </div>
   </div>
 </template>
+
+<style lang="less" scoped>
+@import url("../style/variables.less");
+.contacts {
+  display: flex;
+  justify-content: space-between;
+}
+.info__item {
+  color: @grey;
+  margin-bottom: 30px;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  padding-left: 50px;
+  font-size: 1.2em;
+  p {
+    margin: 4px 0;
+  }
+  .left-icon {
+    position: absolute;
+    font-size: 1.5em;
+    top: 0;
+    left: 0;
+    color: @blue;
+  }
+  .soc-icons {
+    i {
+      opacity: 0.8;
+      transition-duration: 0.3s;
+      font-size: 1.5em;
+      margin-right: 10px;
+      &.fa-whatsapp {
+        color: #11c200;
+      }
+      &.fa-telegram {
+        color: #0088cc;
+      }
+      &:hover {
+        opacity: 1;
+      }
+    }
+  }
+}
+</style>
