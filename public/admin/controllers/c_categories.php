ea3cd6edb17e1325d3382228915ddf05e8a467dc
berryllium <gorkundp@yandex.ru>
2019-12-18 11:49:36 +0300

    редактирование категорий

--- public/admin/controllers/c_categories.php
+++ public/admin/controllers/c_categories.php
@@ -10,11 +10,31 @@ if ($_GET['act'] == 'del') {
 }
 
 if ($_GET['act'] == 'add' && !empty($_POST)) {
-  $catalog->db->Insert('categories', $_POST);
+  $category = $_POST;
+  $category['id'] = $catalog->db->Insert('categories', $category);
+  if($_FILES['img']['name']) {
+    $img = $_FILES['img'];
+    $category['img'] = translit($img['name']);
+    $folder = '../db/images/category/'.$category['id'].'/';
+    $path = $folder.$category['img'];
+    if(!file_exists($folder)) mkdir($folder, 0700, true);
+    move_uploaded_file($img['tmp_name'], $path);
+  }
+  $catalog->db->Update('categories', $category, 'id', $category['id']);
 }
 
 if ($_GET['act'] == 'update' && !empty($_POST)) {
-  $catalog->db->Update('categories', $_POST, 'id', $_POST['id']);
+  $category = $_POST;
+  if($_FILES['img']['name']) {
+    $img = $_FILES['img'];
+    $category['img'] = translit($img['name']);
+    $folder = '../db/images/category/'.$category['id'].'/';
+    $path = $folder.$category['img'];
+    if(!file_exists($folder)) mkdir($folder, 0700, true);
+    move_uploaded_file($img['tmp_name'], $path);
+  }
+
+  $catalog->db->Update('categories', $category, 'id', $category['id']);
 }
 
 $categories = $catalog->db->Select('categories');
