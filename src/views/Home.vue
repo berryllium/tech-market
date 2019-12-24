ee9b16697a95f0b03d38246866a403ec58d85d43
berryllium <gorkundp@yandex.ru>
2019-12-24 23:30:11 +0300

    начал фильтры товаров на фронтенде

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,28 @@
 <template>
   <div class="content">
+    <ul class="filters">
+      <li class="filter">
+        <div class="filter-name">Популярность</div>
+        <select name="filter1">
+          <option value="значение 1">по убыванию</option>
+          <option value="значение 2">по возрастанию</option>
+        </select>
+      </li>
+      <li class="filter">
+        <div class="filter-name">Цена</div>
+        <select name="filter2">
+          <option value="значение 1">по убыванию</option>
+          <option value="значение 2">по возрастанию</option>
+        </select>
+      </li>
+      <li class="filter">
+        <div class="filter-name">Мощность</div>
+        <select name="filter2">
+          <option value="значение 1">по убыванию</option>
+          <option value="значение 2">по возрастанию</option>
+        </select>
+      </li>
+    </ul>
     <div class="products">
       <product v-for="product in this.products" :product="product" :key="product.id" />
     </div>
@@ -30,5 +53,14 @@ export default {
   display: grid;
   gap: 25px;
   grid-template-columns: repeat(4, 1fr);
+}
+.filters {
+  list-style-type: none;
+  padding: 0;
+  display: flex;
+  flex-wrap: wrap;
+}
+.filter {
+  margin-right: 20px;
 }
 </style>
